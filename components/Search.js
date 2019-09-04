a4f3935b2d8f4e24b42369cc4b4fa8a6b475674b
Seb <sebastien.philippe5@gmail.com>
2019-09-04 23:11:51 +0200

    Ajout de la gestion du state de Search pour capturer le champs text et interroger l'api. Récupération de la liste de films et affichage en fonction du texte saisi

--- components/Search.js
+++ components/Search.js
@@ -1,27 +1,48 @@
 import React from 'react'
-import {StyleSheet,View, TextInput, Button,FlatList,Text} from 'react-native'
+import {StyleSheet, View, TextInput, Button, FlatList, Text} from 'react-native'
 import films from '../helpers/filmData'
 import FilmItem from "./FilmItem";
+import {getFilmsfromApiWithSearchedText} from '../api/TMDBapi'
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            films: []
+        }
+        this.searchText= ""
+    }
+
+
+    loadFilm() {
+        if (this.searchText.length > 0)
+            getFilmsfromApiWithSearchedText(this.searchText).then((data) => this.setState({films: data.results}));
+    }
+
+    searchTextInputChange(text) {
+        this.searchText= text
+    }
+
     render() {
         return (
             <View style={styles.main_container}>
-                <TextInput style={styles.textinput} placeholder="Titre du film"/>
+                <TextInput onChangeText={(text) => this.searchTextInputChange(text)} style={styles.textinput}
+                           placeholder="Titre du film"/>
                 <Button style={{height: 2}} title="Rechercher" onPress={() => {
+                    this.loadFilm()
                 }}/>
                 <FlatList style={{marginLeft: 5, marginRight: 5}}
-                    data={films}
-                    keyExtractor={(item)=>item.id}
-                    renderItem={({item}) => <FilmItem film={item} />}
+                          data={this.state.films}
+                          keyExtractor={(item) => item.id}
+                          renderItem={({item}) => <FilmItem film={item}/>}
                 />
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create ( {
-    main_container:{marginTop: 20,flex:1},
+const styles = StyleSheet.create({
+    main_container: {marginTop: 20, flex: 1},
     textinput: {marginLeft: 5, marginRight: 5, height: 50, borderColor: '#ff764e', borderWidth: 2, paddingLeft: 5}
 });
 
