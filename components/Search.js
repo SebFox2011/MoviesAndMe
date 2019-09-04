93b3c149289c8ce86541383306b665d7ed09535e
Seb <sebastien.philippe5@gmail.com>
2019-09-04 18:16:12 +0200

    Ajout du style sur TextInput et Bouton

--- components/Search.js
+++ components/Search.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import {View,TextInput,Button} from 'react-native'
+import {StyleSheet,View, TextInput, Button} from 'react-native'
 
-class Search extends React.Component{
+class Search extends React.Component {
     render() {
         return (
-            <View>
-                <TextInput placeholder="Titre du film" />
-                <Button title="Rechercher" onPress={()=> {} }/>
+            <View style={styles.main_container}>
+                <TextInput style={styles.textinput} placeholder="Titre du film"/>
+                <Button style={{height: 50}} title="Rechercher" onPress={() => {
+                }}/>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create ( {
+    main_container:{marginTop: 20,flex:1},
+    textinput: {marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5}
+});
 
 export default Search
